f9368e8f5c72d2e9e9f11453ae0aa1c534f95457
Duncan R Skilton <dr.skilton@mbro.ac.uk>
2022-01-17 13:14:28 +0000

    edited About

--- app/src/main/java/com/example/soduko/About.java
+++ app/src/main/java/com/example/soduko/About.java
@@ -1,5 +1,14 @@
 package com.example.soduko;
 
-public class About {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
 
 }
