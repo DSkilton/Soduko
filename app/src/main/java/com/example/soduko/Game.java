6dacf65c74e00ad31067172e49484461cfc8eddf
Duncan R Skilton <dr.skilton@mbro.ac.uk>
2022-01-17 16:39:59 +0000

    added PuzzleView and Game

--- app/src/main/java/com/example/soduko/Game.java
+++ app/src/main/java/com/example/soduko/Game.java
@@ -1,4 +1,31 @@
 package com.example.soduko;
 
-public class Game {
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Game extends Activity {
+    public static final String TAG = "Soduko";
+    public static final int DIFFICULTY_EASY = 0;
+
+    private int puzzle[];
+    private int flag[];
+
+    private final static String easyPuzzle = "000010000";
+
+    private PuzzleView puzzleView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate");
+        puzzleView = getPuzzle();
+        flag = getPuzzle();
+        calculateUsedTiles();
+        puzzleView = new PuzzleView(this);
+        setContentView(puzzleView);
+        puzzleView.requestFocus();
+
+    }
+
 }
---
+++ app/src/test/java/com/example/soduko/PuzzleView.java
@@ -0,0 +1,33 @@
+package com.example.soduko;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.View;
+
+public class PuzzleView extends View {
+    public static final String TAG = "Soduko";
+    private int[] flag;
+    private final Game game;
+    private float width, height;
+    private int selX, selY;
+    private final Rect selRect = new Rect();
+
+    public PuzzleView(Context context){
+        super(context);
+        this.game = (Game) context;
+        flag = this.game.getPuzzle();
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldW, int oldH){
+        width = w / 3f;
+        height = h / 3f;
+
+        getRect(selX, selY, selRect);
+        Log.d(TAG, "onSizeChanged: width" + width + ", height " + height);
+        super.onSizeChanged(w, h , oldW, oldH);
+    }
+}
