0810844d86b51a07bdb5e1934ef4e220b9cb28b1
Duncan R Skilton <dr.skilton@mbro.ac.uk>
2022-01-20 13:21:09 +0000

    edited MainActivity

--- app/src/main/java/com/example/soduko/MainActivity.java
+++ app/src/main/java/com/example/soduko/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.soduko;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity, Activity implements View.OnClickListener {
+    private static final String TAG = "Soduko";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View newButton = findViewById(R.id.new_btn);
+        newButton.setOnClickListener(this);
+
+        View aboutButton = findViewById(R.id.about_btn);
+        aboutButton.setOnClickListener(this);
+
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
     }
+
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.about_btn:
+                Intent i = new Intent(this, About.class);
+            startActivity(i);
+            break;
+        }
+    }
+
+
 }
