501bd7185f42f2f8b5d71c4c4864fd09617a303b
Duncan R Skilton <dr.skilton@mbro.ac.uk>
2022-02-01 20:25:03 +0000

    added loops to draw board

--- app/src/main/java/com/example/sudoku/PuzzleView.java
+++ app/src/main/java/com/example/sudoku/PuzzleView.java
@@ -1,6 +1,8 @@
 package com.example.sudoku;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.Log;
 import android.view.View;
@@ -16,7 +18,6 @@ public class PuzzleView extends View {
     public PuzzleView(Context context){
         super(context);
         this.game = (Game) context;
-        flag = this.game.getPuzzle();
         setFocusable(true);
         setFocusableInTouchMode(true);
     }
@@ -31,5 +32,48 @@ public class PuzzleView extends View {
         super.onSizeChanged(w, h , oldW, oldH);
     }
 
+    private void getRect(int x, int y, Rect rect){
+        rect.set((int)(x * width), (int)(y * height), (int)(x * width + width), (int)(y * height + height));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        Paint background = new Paint();
+        background.setColor(getResources().getColor(R.color.puzzle_background));
+        canvas.drawRect(0,0, getWidth(), getHeight(), background);
+
+        Paint dark = new Paint();
+        dark.setColor(getResources().getColor(R.color.puzzle_dark));
+
+        Paint hilite = new Paint();
+        hilite.setColor(getResources().getColor(R.color.puzzle_hilite));
+
+        Paint light = new Paint();
+        light.setColor(getResources().getColor(R.color.puzzle_light));
+
+        Paint rcolor = new Paint();
+        rcolor.setColor(getResources().getColor(R.color.puzzle_red));
+
+        //draw the minor grid lines
+        for(int i = 0; i < 9; i++){
+            canvas.drawLine(0, i * height, getWidth(), i * height, light);
+            canvas.drawLine(0, i * height + 1, getWidth(), i * height + 1, hilite);
+            canvas.drawLine(i * width, 0, i * width, getHeight(), light);
+            canvas.drawLine(i * width + 1, 0, i * width + 1, getHeight(), hilite);
+        }
+
+        //draw the major grid lines
+        for(int i = 0; i < 9; i++){
+            if(i % 3 != 0){
+                continue;
+            }
+            canvas.drawLine(0, i * height, getWidth(), i * height, dark);
+            canvas.drawLine(0, i * height + 1, getWidth(), i * height + 1, hilite);
+            canvas.drawLine(i * width, 0, i * width, getHeight(), dark);
+            canvas.drawLine(i * width + 1, 0, i * width + 1, getHeight(), hilite);
+        }
+
+        
+    }
 
 }
